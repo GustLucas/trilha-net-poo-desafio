225631fcd9f08449a8c453ee22060313827e91de
GustLucas <gustavo.lukas04@gmail.com>
2023-12-14 18:59:49 -0300

    modified:   Models/Iphone.cs 	modified:   Models/Nokia.cs 	modified:   Models/Smartphone.cs 	modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("11989982722", "Iphone 12 Pro MAX", "11111111", 500);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("11981337177", "Nokia 15", "2222222", 256);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
